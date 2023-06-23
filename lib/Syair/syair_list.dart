b2f8e2d6efeaa77a768206efc408e8cf8a56b27d
Akmal18aqil <akmalaqilwahyu@gmail.com>
2023-06-24 01:30:37 +0700

    pdf clear delete

--- lib/Syair/syair_list.dart
+++ lib/Syair/syair_list.dart
@@ -1,31 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
+import 'package:miniawradreborn2/Syair/Detail_syair.dart';
 import 'package:miniawradreborn2/page/page.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
-class grid_page extends StatefulWidget {
-  grid_page({super.key});
-
-  @override
-  State<grid_page> createState() => _grid_pageState();
-}
-
-class _grid_pageState extends State<grid_page> {
-//   final String storagePath = "kitab/lamiyah.pdf"
-
-//   Future<String> getDownloadURL() async {
-//   final firebase_storage.Reference ref =
-//       firebase_storage.FirebaseStorage.instance.ref().child(storagePath);
-//       final String downloadUrl = await ref.getDownloadURL();
-//       return downloadUrl;
-// }
-
-//   Future<String> downloadKitab(String url) async{
-//     final file = await DefaultCacheManager().getSingleFile(url);
-//     return file.path;
-//   }
+class IniKitab extends StatelessWidget {
+  const IniKitab({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -39,239 +21,145 @@ class _grid_pageState extends State<grid_page> {
               TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // const AppBar_normalpage(
-            //   judul: 'Kitab-Kitab',
-            // ),
-            Container(
-              child: Column(
-                children: [
-                  input_kitab(
-                      page_kitab_kiri:
-                          page(judulpage: 'Lamiyah', pdfpage: 'lamiyah.pdf'),
-                      cover_kitab_kiri: 'lamiyah.png',
-                      nama_kitab_kiri: 'Lamiyah',
-                      page_kitab_kanan: page(
-                          judulpage: 'Lisanul Fata',
-                          pdfpage: 'lisanul-fata.pdf'),
-                      cover_kitab_kanan: "lisan11.png",
-                      nama_kitab_kanan: 'Lisanul Fata'),
-                  input_kitab(
-                      page_kitab_kiri: page(
-                          judulpage: "Maladzatul `Aisy",
-                          pdfpage: 'maladzatul.pdf'),
-                      cover_kitab_kiri: 'aishi.png',
-                      nama_kitab_kiri: "Maladzatul 'Aisy",
-                      page_kitab_kanan: page(
-                          judulpage: 'Sang Purnama', pdfpage: 'purnama.pdf'),
-                      cover_kitab_kanan: 'purnama.png',
-                      nama_kitab_kanan: 'Sang Purnama'),
-                  input_kitab(
-                      page_kitab_kiri: page(
-                          judulpage: "`Unwanul Hikam", pdfpage: 'unwanul.pdf'),
-                      cover_kitab_kiri: 'unwan.png',
-                      nama_kitab_kiri: "'Unwanul Hikam",
-                      page_kitab_kanan: page(
-                          judulpage: "Hijrah Nabawi", pdfpage: 'nabawiyah.pdf'),
-                      cover_kitab_kanan: 'nabawiyah.png',
-                      nama_kitab_kanan: "Hijrah Nabawi"),
-                  SizedBox(
-                    height: 40,
-                  )
-                ],
+      body: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: List.generate(images.length, (index) {
+          String judul = images[index]['judul'];
+          String gambar = images[index]['gambar'];
+
+          Future<String> getDownloadURL() async {
+            final firebase_storage.Reference ref = firebase_storage
+                .FirebaseStorage.instance
+                .ref()
+                .child("katalog/$gambar");
+            final String downloadURL = await ref.getDownloadURL();
+            return downloadURL;
+          }
+
+          Future<String> downloadgambar(String url) async {
+            final file = await DefaultCacheManager().getSingleFile(url);
+            return file.path;
+          }
+
+          return GestureDetector(
+            onTap: () {
+              if (judul == "Maladzatul Aishi") {
+                Get.to(Detail_syair(judul: "Maladzatul Aishi", path: "kitab/maladzatul.pdf"));
+              }else if  (judul == "Lamiyah") {
+                Get.to(Detail_syair(judul: "Lamiyah", path: "kitab/lamiyah.pdf"));
+              }else if (judul == "Lisanul Fata") {
+                Get.to(Detail_syair(judul: "Lisanul Fata", path: "kitab/lisanul-fata.pdf"));
+              }else if (judul == "Nabawiyah") {
+                Get.to(Detail_syair(judul: "Nabawiyah", path: "kitab/nabawiyah.pdf"));
+              }else if (judul == "Sang Purnama") {
+                Get.to(Detail_syair(judul: "Sang Purnama", path: "kitab/purnama.pdf"));
+              }else if (judul == "Unwanul Hikam") {
+                Get.to(Detail_syair(judul: "Unwanul Hikam", path: "kitab/unwanul.pdf"));
+              }
+            },
+            child: Card(
+              margin: EdgeInsets.only(left: 30, right: 30, top: 20),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
               ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class input_kitab extends StatelessWidget {
-  final page_kitab_kiri;
-  final String cover_kitab_kiri;
-  final String nama_kitab_kiri;
-  final page_kitab_kanan;
-  final String cover_kitab_kanan;
-  final String nama_kitab_kanan;
-  const input_kitab({
-    super.key,
-    required this.page_kitab_kiri,
-    required this.cover_kitab_kiri,
-    required this.nama_kitab_kiri,
-    required this.page_kitab_kanan,
-    required this.cover_kitab_kanan,
-    required this.nama_kitab_kanan,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        top: 30,
-      ),
-      child: Row(
-        // crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          kitab_kiri(
-              page_kitab_kiri: page_kitab_kiri,
-              cover_kitab_kiri: cover_kitab_kiri,
-              nama_kitab_kiri: nama_kitab_kiri),
-          SizedBox(
-            width: 40,
-          ),
-          kitab_kanan(
-              page_kitab_kanan: page_kitab_kanan,
-              cover_kitab_kanan: cover_kitab_kanan,
-              nama_kitab_kanan: nama_kitab_kanan),
-        ],
-      ),
-    );
-  }
-}
-
-class kitab_kanan extends StatelessWidget {
-  const kitab_kanan({
-    super.key,
-    required this.page_kitab_kanan,
-    required this.cover_kitab_kanan,
-    required this.nama_kitab_kanan,
-  });
-
-  final page_kitab_kanan;
-  final String cover_kitab_kanan;
-  final String nama_kitab_kanan;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Get.to(page_kitab_kanan);
-      },
-      child: Container(
-        width: 100,
-        height: 160,
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromARGB(255, 84, 181, 246),
-                  Color.fromARGB(255, 39, 110, 176),
-                ]),
-            borderRadius: BorderRadius.all(Radius.circular(12)),
-            boxShadow: [
-              BoxShadow(
-                  offset: Offset(0, 0),
-                  color: Color.fromARGB(255, 143, 143, 143),
-                  blurRadius: 5,
-                  spreadRadius: 1)
-            ]),
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 10),
-              width: 72,
-              height: 96,
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(8)),
-                child: Image.asset(
-                  'assets/images/${cover_kitab_kanan}',
-                  fit: BoxFit.fitWidth,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color.fromARGB(255, 84, 181, 246),
+                      Color.fromARGB(255, 39, 110, 176),
+                    ],
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 0),
+                      color: Color.fromARGB(255, 143, 143, 143),
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: FutureBuilder<String>(
+                        future: getDownloadURL(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<String> snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text("Maaf anda sedang Offline",textAlign: TextAlign.center,),
+                                CircularProgressIndicator()
+                              ],
+                            );
+                          }
+                          if (snapshot.hasError) {
+                            return Text('Error: ${snapshot.error}');
+                          }
+                          if (snapshot.hasData) {
+                            return Container(
+                              margin:
+                                  EdgeInsets.only(left: 20, right: 20, top: 1),
+                              child: Image.network(snapshot.data!),
+                            );
+                          }
+                          return CircularProgressIndicator();
+                        },
+                      ),
+                    ),
+                    Text(
+                      judul,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(3, 8, 3, 10),
-              child: Text(
-                '${nama_kitab_kanan}',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontSize: 12,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                ),
-              ),
-            )
-          ],
-        ),
+          );
+        }),
       ),
     );
   }
 }
 
-class kitab_kiri extends StatelessWidget {
-  const kitab_kiri({
-    super.key,
-    required this.page_kitab_kiri,
-    required this.cover_kitab_kiri,
-    required this.nama_kitab_kiri,
-  });
-
-  final page_kitab_kiri;
-  final String cover_kitab_kiri;
-  final String nama_kitab_kiri;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Get.to(page_kitab_kiri);
-      },
-      child: Container(
-        width: 100,
-        height: 160,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromARGB(255, 84, 181, 246),
-                  Color.fromARGB(255, 39, 110, 176),
-                ]),
-            borderRadius: BorderRadius.all(Radius.circular(12)),
-            boxShadow: [
-              BoxShadow(
-                  offset: Offset(0, 0),
-                  color: Color.fromARGB(255, 143, 143, 143),
-                  blurRadius: 5,
-                  spreadRadius: 1)
-            ]),
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 10),
-              width: 72,
-              height: 96,
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-                child: Image.asset(
-                  'assets/images/${cover_kitab_kiri}',
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(3, 8, 3, 10),
-              child: Text(
-                '${nama_kitab_kiri}',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 12,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+List<Map<String, dynamic>> images = [
+  {
+    'judul': 'Maladzatul Aishi',
+    'gambar': 'aishi.png',
+  },
+  {
+    'judul': 'Lamiyah',
+    'gambar': 'lamiyah.png',
+  },
+  {
+    'judul': 'Lisanul Fata',
+    'gambar': 'lisan11.png',
+  },
+  {
+    'judul': 'Nabawiyah',
+    'gambar': 'nabawiyah.png',
+  },
+  {
+    'judul': 'Sang Purnama',
+    'gambar': 'purnama.png',
+  },
+  {
+    'judul': 'Unwanul Hikam',
+    'gambar': 'unwan.png',
+  },
+];
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:miniawradreborn2/Dalaill/dalailul.dart';
+import 'package:miniawradreborn2/Syair/Detail_syair.dart';
+import 'package:miniawradreborn2/Syair/syair_list.dart';
 import 'package:miniawradreborn2/card.dart';
 import 'package:miniawradreborn2/home/Nav_bar.dart';
 import 'package:miniawradreborn2/home/home_screen.dart';
@@ -39,7 +41,7 @@ class _MyAppState extends State<MyApp> {
       return GetMaterialApp(
         title: 'Annur2App',
         theme: ThemeData(),
-        home: navbar(),
+        home: Screen(),
         debugShowCheckedModeBanner: false,
         initialRoute: "/",
         routes: rute,
